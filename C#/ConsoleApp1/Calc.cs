4a93ea531a049c6f9ca5895e421222199167bbd6
Sven <77619070+SvenningIt@users.noreply.github.com>
2026-03-05 12:01:14 +0100

    Add files via upload

---
+++ C#/ConsoleApp1/Calc.cs
@@ -0,0 +1,45 @@
+﻿float num1 = 0; float num2 = 0;
+
+Console.WriteLine("Calc in C#\r");
+Console.WriteLine("------------------------\n");
+
+Console.WriteLine("Type a number, and then press Enter");
+num1 = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Type another number, and then press Enter");
+num2 = Convert.ToSingle(Console.ReadLine());
+
+
+
+// Ask the user to choose an option.
+Console.WriteLine("Choose an option from the following list:");
+Console.WriteLine("\t+ - Add");
+Console.WriteLine("\t- - Subtract");
+Console.WriteLine("\t* - Multiply");
+Console.WriteLine("\t/ - Divide");
+Console.Write("Your option? ");
+
+// Use a switch statement to do the math.
+switch (Console.ReadLine())
+{
+    case "+":
+        Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "-":
+        Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "*":
+        Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "/":
+        while (num2 == 0)
+        {
+            Console.WriteLine("Niet 0 broer, anders krijg je een foutmelding bij delen");
+            num2 = Convert.ToSingle(Console.ReadLine());
+        }
+        Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+// Wait for the user to respond before closing.
+Console.Write("Press any key to close the Calculator console app...");
+Console.ReadKey();
